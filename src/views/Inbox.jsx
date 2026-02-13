d2a0d36b126850a07b4cb8eb968a3476e7105a60
GEORGIOS MOYSIADIS <georgemois23@gmail.com>
2026-02-13 17:44:15 +0200

    fix

--- src/views/Inbox.jsx
+++ src/views/Inbox.jsx
@@ -21,7 +21,7 @@ export default function Inbox() {
   sx={{
     display: 'flex',
     width: '100%',
-    height: '100%',
+    height: "100dvh",  
     minWidth: 0,
     minHeight: 0,
     overflow: 'hidden',
